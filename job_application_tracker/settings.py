e323231e5a55b0c6e7b5750a16f997f3375aead9
Arturo <md.arturo@gmail.com>
2025-12-15 14:57:37 +0100

    Create basic url structure for Users app

--- job_application_tracker/settings.py
+++ job_application_tracker/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'job_application_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def register(request):
+    return render(request, "users/register.html")
+
+def login(request):
+    return HttpResponse("Login page")
+
+def logout(request):
+    return HttpResponse("Logout page")
+
+def profile(request):
+    return HttpResponse("Profile page")
